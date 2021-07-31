09b2432cf972bc125d8e69965348fa6825c8935f
Kent Cho <kent.cho@students.mq.edu.au>
2021-07-31 17:35:58 +1000

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,35 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
+import javax.swing.*;
+import java.awt.*;
 
-        System.out.println("~~~~~");
-        System.out.println("Red vs Blue"); 
-        System.out.println("~~~~~");     
+public class Main extends JFrame {
+    public static void main(String[] args) throws Exception {
+        Main window = new Main();
+    }
+
+    public class Canvas extends JPanel {
+        public Canvas() {
+            setPreferredSize(new Dimension(720, 720));
+        }
+
+        @Override
+        public void paint(Graphics g) {
+            for (int i = 10; i < 710; i += 35) {
+                for (int j = 10; j < 710; j += 35) {
+                    g.setColor(Color.WHITE);
+                    g.fillRect(i, j, 35, 35);
+                    g.setColor(Color.BLACK);
+                    g.drawRect(i, j, 35, 35);
+                }
+            }
+        }
+
+    }
+
+    public Main() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Canvas canvas = new Canvas();
+        this.setContentPane((canvas));
+        this.pack();
+        this.setVisible(true);
     }
 }
