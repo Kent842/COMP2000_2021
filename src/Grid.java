c937981313fd218721b9d90acf2b1736ecf6600f
Kent Cho <kent.cho@students.mq.edu.au>
2021-08-09 01:33:02 +1000

    Used ArrayLists instead of normal Java arrays

--- src/Grid.java
+++ src/Grid.java
@@ -1,23 +1,26 @@
 import java.awt.*;
+import java.util.*;
 
 class Grid {
-    //fields
-    Cell[][] cells = new Cell[20][20];
+    // fields
+    ArrayList<ArrayList<Cell>> cells = new ArrayList<ArrayList<Cell>>();
 
     // constructor
-    public Grid(){
-        for(int i = 0; i < cells.length; i++){
-            for(int j = 0; j < cells[i].length; j++){
-                cells[i][j] = new Cell(10+35*i,10+35*j);
+    public Grid() {
+        for (int i = 0; i < 20; i++) {
+            ArrayList<Cell> cellList = new ArrayList<Cell>();
+            cells.add(cellList);
+            for (int j = 0; j < 20; j++) {
+                cells.get(i).add(new Cell(10 + 35 * i, 10 + 35 * j));
             }
         }
     }
 
     // methods
-    public void paint(Graphics g, Point mousePos){
-        for(int i = 0; i < cells.length; i++){
-            for(int j = 0; j < cells[i].length; j++){
-                cells[i][j].paint(g, mousePos);
+    public void paint(Graphics g, Point mousePos) {
+        for (int i = 0; i < cells.size(); i++) {
+            for (int j = 0; j < cells.get(i).size(); j++) {
+                cells.get(i).get(j).paint(g, mousePos);
             }
         }
     }
--- src/Main.java
+++ src/Main.java
@@ -5,36 +5,36 @@ class Main extends JFrame {
     
     class App extends JPanel {
         
-        Grid grid;
+        Grid grid; //Grid object
 
         public App() {
-            setPreferredSize(new Dimension(720, 720));
-            grid = new Grid();
+            setPreferredSize(new Dimension(720, 720)); //Set window dimensions
+            grid = new Grid(); //Create new Grid object
         }
 
         @Override
         public void paint(Graphics g) {
-            grid.paint(g, getMousePosition());
+            grid.paint(g, getMousePosition()); //Draw Grid, passes in graphics and current mouse position
         }
 
     }
 
     public static void main(String[] args) throws Exception {
-        Main window = new Main();
-        window.run();
+        Main window = new Main(); //Create new window
+        window.run(); //Run window
     }
 
     private Main() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        App canvas = new App();
-        this.setContentPane(canvas);
+        App canvas = new App(); //Create new App called "canvas"
+        this.setContentPane(canvas); //The window's content is going to be showin in "canvas"
         this.pack();
         this.setVisible(true);
     }
 
     public void run() {
         while (true) {
-            this.repaint();
+            this.repaint(); //Constantly refreshes the page's frames
         }
     }
 }
